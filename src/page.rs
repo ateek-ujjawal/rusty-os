1bdc37d3a07cfb172f8ef0d987012587e27274eb
Ateek Ujjawal <ujjawal.a@northeastern.edu>
2024-07-23 05:29:23 -0400

    Implement MMU map and unmap virtual addresses
    Signed-off-by: Ateek Ujjawal <ujjawal.a@northeastern.edu>

--- src/page.rs
+++ src/page.rs
@@ -26,8 +26,8 @@ pub const fn align_val(val: usize, order: usize) -> usize {
 #[repr(u8)]
 pub enum PageBits {
 	Empty = 0,
-	Taken = 1,
-	Last = 2
+	Taken = 1 << 0, // Last bit is 1
+	Last = 1 << 1 // 2nd last bit is 1
 }
 
 // Get flag as an unsigned 8-bit integer
@@ -232,4 +232,160 @@ pub fn print_page_allocations() {
         println!("Free: {:>6} pages ({:>10} bytes).", (num_pages - num), (num_pages - num) * PAGE_SIZE);
         println!();
     }
+}
+
+// MMU
+
+// Represent the last 8 bits of the page table entry
+// Describes various flags
+#[repr(i64)]
+#[derive(Clone, Copy)]
+pub enum EntryBits {
+	None = 0,
+	Valid = 1 << 0,
+	Read = 1 << 1,
+	Write = 1 << 2,
+	Execute = 1 << 3,
+	User = 1 << 4,
+	Global = 1 << 5,
+	Access = 1 << 6,
+	Dirty = 1 << 7
+}
+
+impl EntryBits {
+	pub fn val(self) -> i64 {
+		self as i64
+	}
+}
+
+// One page table entry has 64 bits
+// Leaf PTE means no need to walk through the tables, since physical page(memory) has been found
+pub struct Entry {
+	pub entry: i64
+}
+
+impl Entry {
+	pub fn get_entry(&self) -> i64 {
+		self.entry
+	}
+
+	pub fn set_entry(&mut self, entry: i64) {
+		self.entry = entry;
+	}
+
+	pub fn is_valid(&self) -> bool {
+		self.get_entry() & EntryBits::Valid.val() != 0
+	}
+
+	pub fn is_invalid(&self) -> bool {
+		!self.is_valid()
+	}
+
+	// Check if this is leaf PTE(Read, write or execute bits are 1)
+	pub fn is_leaf(&self) -> bool {
+		self.get_entry() & 0xe != 0
+	}
+
+	pub fn is_branch(&self) -> bool {
+		!self.is_leaf()
+	}	
+}
+
+// Define a page table
+// Has 512 entries, each of 8 bytes, giving 512 * 8 = 4096 bytes for one table
+pub struct Table {
+	pub entries: [Entry; 512]
+}
+
+impl Table {
+	pub fn len() -> usize {
+		512
+	}
+}
+
+// Maps a virtual address to the given physical address
+// root: Root page table
+// vaddr: The virtual address as specified in RISC-V privileged isa
+// paddr: The physical address as specified in RISC-V privileged isa
+// bits: The 8 entry bits to be set in the page table entry
+// level: The levels needed to traverse the page tables to locate the physical address
+pub fn map(root: &mut Table, vaddr: usize, paddr: usize, bits: i64, level: usize) {
+	// The bits to be set, must have either read, write or execute bit set
+	// otherwise this will be a faulty page
+	assert!(bits & 0xe != 0);
+
+	// Get the virtual page numbers
+	let vpn = [
+		vaddr >> 12, // VPN[0]
+		vaddr >> 21, // VPN[1]
+		vaddr >> 30  // VPN[2]
+	];
+
+	// Get the physical page numbers from the physical address
+	let ppn = [
+		paddr >> 12, // PPN[0]
+		paddr >> 21, // PPN[1]
+		paddr >> 30  // PPN[2]
+	];
+
+	// Get root page table entry
+	let mut v = &mut root.entries[vpn[2]];
+
+	// Loop through the levels of the page tables
+	// Reverse to loop backwards from level 2
+	for i in (level..2).rev() {
+		if v.is_invalid() {
+			// Valid page table entry not found
+			// So allocate a physical page and store it in the entry
+			let page_addr = zalloc(1);
+
+			// Get the page address, convert it to an i64 number
+			// In the Sv39 scheme, physical addresses start at bit 12 (11:0 reserved for the offset)
+			// While the same physical address starts at bit 10 in the page table entry (9:0 reserved for various entry bit flags)
+			// So we shift the physical address we get from the hardware MMU by 2 bits to ensure correct alignment
+			v.set_entry((page_addr as i64 >> 2) | EntryBits::Valid.val());
+		}
+
+		// Get the physical address of next page from the entry and shift it left by 2 to fit into the physical address space(56-bits)
+		// as defined in the Sv39 scheme
+		// We bitwise and with !0x3ff so that the last 10 bits are zeroed out(we don't need them in branches)
+		let entry = ((v.get_entry() & !0x3ff) << 2) as *mut Entry;
+		v = unsafe { entry.add(vpn[i]).as_mut().unwrap() };
+	}
+
+	// We reach here when we find a leaf(v points to a physical page that is not a page table)
+	// Map the given physical page in this entry
+	let entry = (ppn[2] << 28) as i64 |
+					 (ppn[1] << 19) as i64 |
+					 (ppn[0] << 10) as i64 |
+					  bits |					// set provided bits
+					  EntryBits::Valid.val();	// Valid page
+	v.set_entry(entry);
+}
+
+// Unmap/deallocate page tables for later use
+pub fn unmap(root: &mut Table) {
+	// Loop 512 page table entries in the root table
+	// which will give page tables corresponding to level 2
+	for lv2 in 0..Table::len() {
+		let ref entry_lv2 = root.entries[lv2];
+		// Check if given entry is valid and is a branch
+		if entry_lv2.is_valid() && entry_lv2.is_branch() {
+			// Get address of level 1 page table
+			let memaddr_lv1 = (entry_lv2.get_entry() & !0x3ff) << 2;
+			let table_lv1 = unsafe { (memaddr_lv1 as *mut Table).as_mut().unwrap() };
+			for lv1 in 0..Table::len() {
+				let ref entry_lv1 = table_lv1.entries[lv1];
+				if entry_lv1.is_valid() && entry_lv1.is_branch() {
+					// Get address of level 0 page table
+					let memaddr_lv0 = (entry_lv1.get_entry() & !0x3ff) << 2;
+
+					// Free the memory address(page), since branches won't exist at level 0
+					dealloc(memaddr_lv0 as *mut u8);
+				}
+			}
+			// Free the level 1 page table after freeing the tables inside it
+			dealloc(memaddr_lv1 as *mut u8);
+		}
+	}
 }
